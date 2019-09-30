ede56b7836b64ad1a7613bf114b520e9cfe32615
qiawu <wu7847400>
2019-09-30 17:35:53 +0800

    leedCode

--- src/leedcode/Test15.java
+++ src/leedcode/Test15.java
@@ -15,15 +15,42 @@ import java.util.*;
 public class Test15 {
 
 	public static void main(String[] args) {
-		System.out.println(JSON.toJSON(threeSum(new int[]{-1, 0, 1, 2, -1, -4})));
-		System.out.println(JSON.toJSON(threeSum(new int[]{0, 0, 0, 0})));
-		System.out.println(JSON.toJSON(threeSum(new int[]{1,1,-2})));
+		System.out.println(JSON.toJSON(threeSumTwo(new int[]{-1, 0, 1, 2, -1, -4})));
+		System.out.println(JSON.toJSON(threeSumTwo(new int[]{0, 0, 0, 0})));
+		System.out.println(JSON.toJSON(threeSumTwo(new int[]{1,1,-2})));
 	}
 
 	public static List<List<Integer>> threeSumTwo(int[] nums) {
-		HashMap<Integer, HashMap<String,List<Integer>>> map = new HashMap();
 		List<List<Integer>> result = new ArrayList<>();
 		Arrays.sort(nums);
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] > 0) {
+				break;
+			}
+			if (i > 0 && nums[i] == nums[i-1]) {
+				continue;
+			}
+			int left = i + 1;
+			int right = nums.length - 1;
+			while (left < right){
+				int sum = nums[i] + nums [left] + nums[right];
+				if (sum == 0) {
+					result.add(Arrays.asList(nums[i] , nums [left] , nums[right]));
+					while(left < right && nums[left] == nums[left + 1]) {
+						left ++;
+					}
+					while (left < right && nums[right] == nums [right -1]) {
+						right --;
+					}
+					left ++;
+					right --;
+				} else if (sum > 0) {
+					right -- ;
+				} else if (sum < 0) {
+					left ++;
+				}
+			}
+		}
 		return result;
 	}
 
